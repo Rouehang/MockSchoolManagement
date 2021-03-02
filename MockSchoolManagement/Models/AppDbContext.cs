38e1a547f4f774bf971d7a12efc2d5e5ab32c1e3
Zehang <643153778@qq.com>
2021-03-02 15:47:15 +0800

    48.在 Entity Framework Core 中使用 Sql server

---
+++ MockSchoolManagement/Models/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.Models
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// 学生类
+        /// </summary>
+        public DbSet<Student> students { get; set; }
+
+
+
+
+
+
+
+    }
+}
